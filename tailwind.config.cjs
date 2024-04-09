a72ad052a38065105d7fb85d0797f5d30c2f02b9
Ariel <arielgarciarodriguez004@gmail.com>
2024-04-09 17:09:09 -0400

    Added Tailwind and Dark Mode Configuration

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'selector',
+    content: ["./src/**/*.{html,js,jsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
